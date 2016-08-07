85c59b1a0df5e2011de72e9e2c9755423e33493e
George Marzloff <george@marzloffmedia.com>
2016-08-07 17:49:34 -0400

    click and back functionality correct

--- JavaScript/dynamicfood.js
+++ JavaScript/dynamicfood.js
@@ -86,7 +86,7 @@ window.onload = function (){
 	var daysInBlog = [juneEighteen, juneNineteen, juneTwenty, juneTwentyTwo, juneTwentyTwo, juneTwentyThree, juneTwentyFour, juneTwentyFive, juneTwentySix]
 
 	function makeFoodHTML(date){
-		return '<div class="foodDayContainer"> ' +
+		return '<div class="foodDayContainer"> ' + date.title + 
 					'<ul>'+
 						'<li>'+
 							'<img src="' + date.meals.breakfast.img + '" />'+
@@ -117,12 +117,37 @@ window.onload = function (){
 		}
 		return dayString;
 	}
+	
+	$(".month")[0].innerHTML = makeTitle(daysInBlog[0]);
+	$("#mealDataContainer").html(makeHTMLForAllDays(daysInBlog));
 
-	document.getElementsByClassName("month")[0].innerHTML = makeTitle(daysInBlog[0]);
-	document.getElementById("foodContainer").innerHTML = makeHTMLForAllDays(daysInBlog);
+	var dayDivs = $('#mealDataContainer').children('.foodDayContainer')
+console.log("dayDivs count: " + dayDivs.length);
+
+	$(".days .dateCell").click(function(){
+
+		$("#backButton").removeClass("hide");
+		$("#calendar").addClass("hide");
+		$("#mealDataContainer").removeClass("hide");
+
+		var buttonIndex = $(this).index() - $(".days .empty").length;
+		// the array $(".days .dateCell") returns li's that maintain their original index() positions
+		// corrects for empty li's at beginning.
+
+		console.log('index clicked: ' + buttonIndex);
+		
+		var activeDay = dayDivs.eq(buttonIndex);
+		activeDay.removeClass("hide");
+		dayDivs.not(activeDay).addClass("hide");
+		// debugger;
+	});
+
+	$("#backButton").click(function(){
+		$("#calendar").removeClass("hide");
+		$("#mealDataContainer").addClass("hide");
+	});
+
 }
-
-// var dayDivs = $('foodContainer').children('.foodDayContainer')
 
 
 
